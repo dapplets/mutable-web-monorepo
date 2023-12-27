d284852f6d4455b4ba005620eb661d6e48f6eaf2
Alexander Sakhaev <alsakhaev@gmail.com>
2023-12-27 11:15:19 +0100

    feat: inject BOS-components with Shadow DOM (DAP-4272)

--- src/contentscript/index.ts
+++ src/contentscript/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello from content script!")
+import '@near-wallet-selector/modal-ui/styles.css'
+
+import { Engine } from 'mutable-web-engine'
+
+const NetworkId = 'mainnet'
+
+async function main() {
+  const engine = new Engine({
+    networkId: NetworkId,
+  })
+  await engine.start()
+
+  console.log('Mutable Web Engine started!', engine)
+}
+
+main().catch(console.error)
