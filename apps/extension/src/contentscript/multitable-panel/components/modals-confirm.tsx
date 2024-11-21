b90e2df5535d46cc12ddc03d793c0f336f1d3e5b
Lisofffa <lisofffa@gmail.com>
2024-11-21 03:41:24 +0300

    feat: added version to modal publish

--- apps/extension/src/contentscript/multitable-panel/components/modals-confirm.tsx
+++ apps/extension/src/contentscript/multitable-panel/components/modals-confirm.tsx
@@ -231,7 +231,7 @@ export const ModalConfirm: FC<Props> = ({
   loggedInAccountId,
 }) => {
   const { name, image, description, fork_of } = editingMutation.metadata
-
+  const { version } = editingMutation
   // Close modal with escape key
   useEscape(onCloseCurrent) // ToDo -- does not work
 
@@ -378,7 +378,10 @@ export const ModalConfirm: FC<Props> = ({
               </StyledLabel>
             </FloatingLabelContainer>
           </CardWrapper>
-
+          <FloatingLabelContainer>
+            <StyledInput id={'version'} type={'text'} value={version} readOnly />
+            <StyledLabel htmlFor={'version'}>Version</StyledLabel>
+          </FloatingLabelContainer>
           <FloatingLabelContainerArea>
             <StyledTextarea
               id={'description'}
@@ -444,7 +447,10 @@ export const ModalConfirm: FC<Props> = ({
               </StyledLabel>
             </FloatingLabelContainer>
           </CardWrapper>
-
+          <FloatingLabelContainer>
+            <StyledInput id={'version'} type={'text'} value={version} readOnly />
+            <StyledLabel htmlFor={'version'}>Version</StyledLabel>
+          </FloatingLabelContainer>
           <FloatingLabelContainerArea>
             <StyledTextarea
               id={'description'}
@@ -472,7 +478,10 @@ export const ModalConfirm: FC<Props> = ({
               <span>by me ({loggedInAccountId})</span>
             </TextWrapper>
           </CardWrapper>
-
+          <FloatingLabelContainer>
+            <StyledInput id={'version'} type={'text'} value={version} readOnly />
+            <StyledLabel htmlFor={'version'}>Version</StyledLabel>
+          </FloatingLabelContainer>
           {forkedMutation ? (
             <>
               <Label>Originally Forked from</Label>
