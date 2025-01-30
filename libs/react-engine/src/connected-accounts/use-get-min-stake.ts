ad06a7c1a212d87c215b07fda45d3e0ad5b07ffb
Alexander Sakhaev <alsakhaev@gmail.com>
2025-01-30 15:50:36 +0300

    refactor: rename hook

--- libs/react-engine/src/connected-accounts/use-get-min-stake.ts
+++ libs/react-engine/src/connected-accounts/use-get-min-stake.ts
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query'
 import { useEngine } from '../engine'
 
-export function useMinStakeAmount() {
+export function useGetMinStake() {
   const { engine } = useEngine()
 
   const {
