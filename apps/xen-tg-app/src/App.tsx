65e45c575da941291610ade5bd4acfe00a3aa199
Nikita Teremovskiy <ni-2@mail.ru>
2025-04-17 10:53:18 +0300

    feat: add animated background; add prettier config

--- apps/xen-tg-app/src/App.tsx
+++ apps/xen-tg-app/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import XEN_IMAGE from './assets/xen-girl-001.png'
 
 type TUserInfo = {
   id: number
@@ -29,27 +30,56 @@ function App() {
   }, [])
 
   return (
-    <div className="flex flex-col gap-6 p-7 justify-center items-center">
-      <h1 className="text-3xl font-bold">Welcome to Xen Telegram App</h1>
-      {user ? (
-        <div className="flex flex-col gap-2 p-7 justify-center items-center">
-          <p>
-            <strong>ID:</strong> ${user.id}
-          </p>
-          <p>
-            <strong>First Name:</strong> ${user.firstName}
-          </p>
-          <p>
-            <strong>Last Name:</strong> ${user.lastName ? user.lastName : '-'}
-          </p>
-          <p>
-            <strong>Username:</strong> ${user.username ? user.username : '-'}
-          </p>
+    <>
+      <div className="gradient-bg">
+        <svg xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <filter id="goo">
+              <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+              <feColorMatrix
+                in="blur"
+                mode="matrix"
+                values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
+                result="goo"
+              />
+              <feBlend in="SourceGraphic" in2="goo" />
+            </filter>
+          </defs>
+        </svg>
+        <div className="gradients-container">
+          <div className="g1"></div>
+          <div className="g2"></div>
+          <div className="g3"></div>
+          <div className="g4"></div>
+          <div className="g5"></div>
+          <div className="interactive"></div>
         </div>
-      ) : (
-        <p>User information is not available.</p>
-      )}
-    </div>
+      </div>
+      <div className="relative flex flex-col items-center justify-center gap-6 p-8 text-[var(--color-white-text)]">
+        <div className="flex w-[210px] justify-center overflow-hidden rounded-full">
+          <img src={XEN_IMAGE} alt="xen-photo" className="h-full w-full" />
+        </div>
+        <h1 className="text-center text-3xl font-bold">Welcome to Xen Telegram App</h1>
+        {user ? (
+          <div className="flex flex-col items-center justify-center gap-2 p-7">
+            <p className="text-center">
+              <strong>ID:</strong> ${user.id}
+            </p>
+            <p className="text-center">
+              <strong>First Name:</strong> ${user.firstName}
+            </p>
+            <p className="text-center">
+              <strong>Last Name:</strong> ${user.lastName ? user.lastName : '-'}
+            </p>
+            <p className="text-center">
+              <strong>Username:</strong> ${user.username ? user.username : '-'}
+            </p>
+          </div>
+        ) : (
+          <p className="text-center">User information is not available.</p>
+        )}
+      </div>
+    </>
   )
 }
 
