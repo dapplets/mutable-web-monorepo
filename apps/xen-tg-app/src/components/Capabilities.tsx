6f4de33a10655a77c0f7e9a0ec418a18f5648e4e
Nikita Teremovskiy <ni-2@mail.ru>
2025-04-25 14:21:39 +0300

    fix: link to Near AI agents in default browser

--- apps/xen-tg-app/src/components/Capabilities.tsx
+++ apps/xen-tg-app/src/components/Capabilities.tsx
@@ -40,16 +40,19 @@ const Capabilities: FC<TCapabilitiesProps> = ({ user }) => {
     console.log('disconnect agent', id)
   }
 
+  const openNearAI = () => window.Telegram.WebApp.openLink('https://app.near.ai/agents')
+
   return (
     <div className="z-1 flex w-full flex-col items-center justify-between gap-2.5 rounded-xl border border-[#f8f9ff66] p-2.5 backdrop-blur-3xl backdrop-opacity-80">
       <div className="my-1.5 flex w-full items-center justify-between">
         <h1 className="text-center text-2xl font-bold">Capabilities</h1>
-        <a
+        <button
+          role="link"
           className="me-3 flex cursor-pointer p-1.5 text-[#7A818B] transition hover:text-(--color-main-text)"
-          href="https://near.ai" // ToDo: add link
+          onClick={openNearAI}
         >
           <ExternalLinkIcon />
-        </a>
+        </button>
       </div>
       {MOCKED_DATA.agents.map((agent) => (
         <Agent key={agent.name} agent={agent} onDisconnect={() => handleDisconnect(agent.name)} />
