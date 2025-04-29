a45d812925f03ef6206cfca0ae4252a3c3c3cd78
Nikita Teremovskiy <ni-2@mail.ru>
2025-04-29 17:28:58 +0300

    feat: change authorization

--- apps/xen-tg-app/src/components/Memory.tsx
+++ apps/xen-tg-app/src/components/Memory.tsx
@@ -13,7 +13,7 @@ const mutationFn = (name: string, params?: { [key: string]: string | number }) =
   const response = await fetch(API_URL, {
     method: 'POST',
     headers: {
-      Authorization: `Bearer ${JSON.stringify(window.Telegram.WebApp.initDataUnsafe)}`,
+      Authorization: `Bearer ${window.Telegram.WebApp.initData}`,
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
