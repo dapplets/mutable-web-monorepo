fa047df5308d90d8fc007d6b389548d9454285b9
Nikita Teremovskiy <ni-2@mail.ru>
2025-04-30 12:29:03 +0300

    fix: substitute the `tgDataStr` paramter with `window.Telegram.WebApp.initData`

--- apps/xen-tg-app/src/App.tsx
+++ apps/xen-tg-app/src/App.tsx
@@ -10,36 +10,34 @@ import Warnings from './components/Warnings'
 import { TMemory } from './types'
 import { API_URL } from './env'
 
-const queryFn =
-  (tgDataStr: string, name: string, params?: { [key: string]: string | number }) => async () => {
-    if (!tgDataStr) {
-      throw new Error('Telegram is not available')
-    }
-    const response = await fetch(API_URL, {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${tgDataStr}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        jsonrpc: '2.0',
-        method: name,
-        params: params ?? {},
-        id: 1,
-      }),
-    })
-    if (!response.ok) {
-      throw new Error('Network response was not ok')
-    }
-    const data = await response.json()
-    return data.result
+const queryFn = (name: string, params?: { [key: string]: string | number }) => async () => {
+  if (!window.Telegram.WebApp.initData) {
+    throw new Error('Telegram is not available')
   }
+  const response = await fetch(API_URL, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${window.Telegram.WebApp.initData}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      jsonrpc: '2.0',
+      method: name,
+      params: params ?? {},
+      id: 1,
+    }),
+  })
+  if (!response.ok) {
+    throw new Error('Network response was not ok')
+  }
+  const data = await response.json()
+  return data.result
+}
 
 function App() {
-  const tgDataStr = window.Telegram.WebApp.initData
   const { data: memories } = useQuery<{ items: TMemory[]; total: number }>({
-    queryKey: ['memories', tgDataStr],
-    queryFn: queryFn(tgDataStr, 'getMemories', {
+    queryKey: ['memories'],
+    queryFn: queryFn('getMemories', {
       offset: 0,
       limit: 10,
     }),
