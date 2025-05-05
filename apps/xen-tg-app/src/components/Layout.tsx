486817a0970d84e67c5b8ecf1bc250ffd353e5cf
Nikita Teremovskiy <ni-2@mail.ru>
2025-05-05 17:46:19 +0300

    feat: big buttons, system scheme (issue#187)

--- apps/xen-tg-app/src/components/Layout.tsx
+++ apps/xen-tg-app/src/components/Layout.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react'
 import AnimatedBackground from './AnimatedBackground'
 
 const Layout: FC<{ children: React.ReactNode }> = ({ children }) => (
-  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+  <ThemeProvider storageKey="vite-ui-theme">
     <div className="relative flex w-full max-w-xl min-w-80 flex-col items-center justify-center gap-5 px-2.5 py-5 text-(--color-main-text)">
       <AnimatedBackground />
       {children}
