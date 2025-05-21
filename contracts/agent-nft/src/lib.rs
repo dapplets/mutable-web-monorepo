0bf278e62afdd6d10690758f3e101a5c017cd109
Alexander Sakhaev <alsakhaev@gmail.com>
2025-05-21 16:00:12 +0300

    feat(agent-nft): implement NEP-199 as stub

--- contracts/agent-nft/src/lib.rs
+++ contracts/agent-nft/src/lib.rs
@@ -27,8 +27,10 @@ use near_contract_standards::non_fungible_token::NonFungibleToken;
 use near_contract_standards::non_fungible_token::{Token, TokenId};
 use near_sdk::collections::LazyOption;
 use near_sdk::json_types::U128;
+use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::{
-    env, near, require, AccountId, BorshStorageKey, PanicOnDefault, Promise, PromiseOrValue,
+    assert_one_yocto, env, near, require, serde_json::json, AccountId, BorshStorageKey,
+    PanicOnDefault, Promise, PromiseOrValue,
 };
 use std::collections::HashMap;
 
@@ -37,6 +39,13 @@ use std::collections::HashMap;
 pub struct Contract {
     tokens: NonFungibleToken,
     metadata: LazyOption<NFTContractMetadata>,
+}
+
+// WARNING! We are not using royalties, but want to be compatible with existing marketplaces
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Payout {
+    pub payout: HashMap<AccountId, U128>,
 }
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
@@ -107,6 +116,62 @@ impl Contract {
         );
         self.tokens
             .internal_mint(token_id, token_owner_id, Some(token_metadata))
+    }
+
+    // WARNING! We are not using royalties, but want to be compatible with existing marketplaces
+    fn nft_payout(&self, token_id: String, balance: U128, max_len_payout: u32) -> Payout {
+        let owner_id = self
+            .tokens
+            .owner_by_id
+            .get(&token_id)
+            .expect("Token id does not exist");
+
+        let mut result: HashMap<AccountId, U128> = HashMap::new();
+        result.insert(owner_id, balance);
+
+        Payout { payout: result }
+    }
+
+    // WARNING! We are not using royalties, but want to be compatible with existing marketplaces
+    #[payable]
+    pub fn nft_transfer_payout(
+        &mut self,
+        receiver_id: AccountId,
+        token_id: String,
+        approval_id: u64,
+        balance: U128,
+        max_len_payout: u32,
+    ) -> HashMap<AccountId, U128> {
+        assert_one_yocto();
+
+        let owner_id = self
+            .tokens
+            .owner_by_id
+            .get(&token_id)
+            .expect("Token id does not exist");
+        self.tokens.nft_transfer(
+            receiver_id.clone(),
+            token_id.clone(),
+            Some(approval_id),
+            None,
+        );
+
+        env::log(
+            json!({
+                "type": "nft_transfer",
+                "params": {
+                    "token_id": token_id,
+                    "sender_id": owner_id,
+                    "receiver_id": receiver_id,
+                }
+            })
+            .to_string()
+            .as_bytes(),
+        );
+
+        let mut result: HashMap<AccountId, U128> = HashMap::new();
+        result.insert(owner_id, balance);
+        result
     }
 }
 
