1826331cf5bd723e2f8fc41c3026b4d331772091
Alexander Sakhaev <alsakhaev@gmail.com>
2025-06-17 21:12:45 +0300

    feat(near-ai-client): introduce SDK to communicate with NEAR AI API generated by `openapi-generator`

---
+++ libs/near-ai-client/src/apis/index.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+// export * from './AgentsApi';
+// export * from './AgentsAssistantsApi';
+export * from './AssistantsApi';
+export * from './BenchmarkApi';
+export * from './DefaultApi';
+export * from './DelegationApi';
+export * from './EvaluationApi';
+export * from './FilesApi';
+export * from './HubSecretsApi';
+export * from './JobsApi';
+export * from './LogsApi';
+export * from './PermissionsApi';
+export * from './RegistryApi';
+export * from './RunScheduleApi';
+export * from './StarsApi';
+export * from './ThreadsApi';
+export * from './VectorStoresApi';
