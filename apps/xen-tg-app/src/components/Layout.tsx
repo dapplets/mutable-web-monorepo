7b6aac4a7661c042c931daa80c8ac109eeba7555
Nikita Teremovskiy <ni-2@mail.ru>
2025-06-20 20:55:16 +0300

    fix: Layout top margin

--- apps/xen-tg-app/src/components/Layout.tsx
+++ apps/xen-tg-app/src/components/Layout.tsx
@@ -2,19 +2,16 @@ import { ThemeProvider } from '@/components/theme-provider'
 import { FC } from 'react'
 import AnimatedBackground from './AnimatedBackground'
 
-const Layout: FC<{ children: React.ReactNode }> = ({ children }) => {
-  const { contentSafeAreaInset } = window.Telegram.WebApp as any // eslint-disable-line -- wait for a new version of @types/telegram-web-app
-  return (
-    <ThemeProvider storageKey="vite-ui-theme">
-      <div
-        style={{ marginTop: contentSafeAreaInset ? contentSafeAreaInset.top + 14 : 60 }}
-        className="relative flex w-full max-w-xl min-w-80 flex-col items-center justify-center gap-5 px-2.5 py-5 text-(--color-main-text)"
-      >
-        <AnimatedBackground />
-        {children}
-      </div>
-    </ThemeProvider>
-  )
-}
+const Layout: FC<{ children: React.ReactNode }> = ({ children }) => (
+  <ThemeProvider storageKey="vite-ui-theme">
+    <div
+      style={{ marginTop: 'calc(var(--tg-content-safe-area-inset-top) + 12px)' }}
+      className="relative flex w-full max-w-xl min-w-80 flex-col items-center justify-center gap-5 px-2.5 py-5 text-(--color-main-text)"
+    >
+      <AnimatedBackground />
+      {children}
+    </div>
+  </ThemeProvider>
+)
 
 export default Layout
