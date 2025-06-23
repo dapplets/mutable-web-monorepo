79fe8206ddb0907e0efd1f9a7df80a5a34dc2e60
Alexander Sakhaev <alsakhaev@gmail.com>
2025-06-23 16:48:58 +0300

    feat: install community packages by default

---
+++ apps/aigency-backend/init.sh
@@ -0,0 +1,7 @@
+# install community nodes
+mkdir /home/node/.n8n/nodes
+cd /home/node/.n8n/nodes
+npm i @dapplets/n8n-nodes-near-protocol
+npm i @dapplets/n8n-nodes-telegram-utils
+npm i @dapplets/n8n-nodes-telegram-user
+cd ~
