4e4e4962573e847c909260c84453fd05af9eb44b
Alexander Sakhaev <alsakhaev@gmail.com>
2025-06-23 18:57:01 +0300

    build: fix docker entrypoint

--- apps/aigency-backend/docker-entrypoint.sh
+++ apps/aigency-backend/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+
 echo "Running migrations..."
-node ./apps/aigency-backend/node_modules/typeorm/cli.js migration:run -d ./apps/aigency-backend/dist/typeorm.config.js
+node ./node_modules/typeorm/cli.js migration:run -d ./dist/typeorm.config.js
 
 echo "Starting the app..."
-node ./apps/aigency-backend/dist/main.js
+node ./dist/main.js
