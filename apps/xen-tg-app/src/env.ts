83b744cedc087a8871e488daeefb791e77dfefb0
Alexander Sakhaev <alsakhaev@gmail.com>
2025-06-23 23:47:22 +0300

    docs: add env.example for xen-tg-app

--- apps/xen-tg-app/src/env.ts
+++ apps/xen-tg-app/src/env.ts
@@ -1,2 +1,2 @@
 // @ts-ignore
-export const API_URL = import.meta.env.VITE_API_URL ?? 'https://n8n.aigency.test.dapplets.org/webhook/rpc'
+export const API_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:3000/rpc'
