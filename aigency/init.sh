f9db289e4a974b523102ca4897fe6edf9fd0416f
Alexander Sakhaev <alsakhaev@gmail.com>
2025-06-26 01:39:44 +0300

    feat(aigency): add empty open ai credentials to n8n

--- aigency/init.sh
+++ aigency/init.sh
@@ -3,3 +3,12 @@ mkdir /home/node/.n8n/nodes
 cd /home/node/.n8n/nodes
 npm i @dapplets/n8n-nodes-aigency
 cd ~
+
+# import credentials
+n8n import:credentials --input=/backup/credentials.json
+
+# import workflows
+n8n import:workflow --input=/backup/workflows.json
+
+# activate workflows that are active
+jq -r '.[] | select(.active == true) | .id' /backup/workflows.json | xargs -I {} n8n update:workflow --id={} --active=true
