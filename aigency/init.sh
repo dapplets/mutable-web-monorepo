67c9be7949f4a0717c7251f17665283f70d3f8d0
Alexander Sakhaev <alsakhaev@gmail.com>
2025-06-26 17:28:14 +0300

    feat(aigency): initialize credentials using env variables

--- aigency/init.sh
+++ aigency/init.sh
@@ -1,11 +1,18 @@
-# install community nodes
+# install latest community nodes
 mkdir /home/node/.n8n/nodes
 cd /home/node/.n8n/nodes
-npm i @dapplets/n8n-nodes-aigency
+npm i @dapplets/n8n-nodes-aigency@latest
 cd ~
 
+cp /backup/credentials.json /home/node/credentials.json
+
+# fill credentials from env variables
+sed -i "s/\$OPENAI_API_KEY/${OPENAI_API_KEY}/g" /home/node/credentials.json
+sed -i "s/\$AIGENCY_API_KEY/${AIGENCY_API_KEY}/g" /home/node/credentials.json
+sed -i "s/\$AIGENCY_API_URL/${AIGENCY_API_URL}/g" /home/node/credentials.json
+
 # import credentials
-n8n import:credentials --input=/backup/credentials.json
+n8n import:credentials --input=/home/node/credentials.json
 
 # import workflows
 n8n import:workflow --input=/backup/workflows.json
