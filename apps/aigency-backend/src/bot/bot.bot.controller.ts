c9f88087fa27e9e55ea5eb13e0f875064d644e28
Alexander Sakhaev <alsakhaev@gmail.com>
2025-06-27 00:51:23 +0300

    feat(aigency-backend): handle errors on message processing

--- apps/aigency-backend/src/bot/bot.bot.controller.ts
+++ apps/aigency-backend/src/bot/bot.bot.controller.ts
@@ -1,5 +1,5 @@
 import { Update, On, Message, Ctx } from 'nestjs-telegraf';
-import { UseFilters } from '@nestjs/common';
+import { Logger, UseFilters } from '@nestjs/common';
 import { AllExceptionsFilter } from '../common/all-exceptions.filter';
 import { BotService } from './bot.service';
 import { TelegrafContext } from 'src/common/telegraf-context.interface';
@@ -7,6 +7,8 @@ import { TelegrafContext } from 'src/common/telegraf-context.interface';
 @UseFilters(AllExceptionsFilter)
 @Update()
 export class BotBotController {
+  private readonly logger = new Logger(BotBotController.name);
+
   constructor(private botService: BotService) {}
 
   @On('message')
@@ -15,10 +17,16 @@ export class BotBotController {
     @Ctx() ctx: TelegrafContext,
   ): Promise<string | undefined> {
     if (!ctx.chat?.id) {
-      return JSON.stringify({ error: 'Chat ID not found' });
+      return 'Chat ID not found';
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    await this.botService.processMessage(ctx.chat.id, message);
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      await this.botService.processMessage(ctx.chat.id, message);
+    } catch (error) {
+      this.logger.error(error);
+
+      return 'Error processing message. There is probably no connection to N8N.';
+    }
   }
 }
