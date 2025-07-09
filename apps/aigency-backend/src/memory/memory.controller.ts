cdd44b0cec4872fa18e14f99044208310737ac6a
Nikita Teremovskiy <ni-2@mail.ru>
2025-07-09 18:02:31 +0300

    feat: add `getMemoriesByUser` API method

--- apps/aigency-backend/src/memory/memory.controller.ts
+++ apps/aigency-backend/src/memory/memory.controller.ts
@@ -8,11 +8,12 @@ import { z } from 'zod';
 import { AllRpcExceptionsFilter } from 'src/common/all-rpc-exceptions.filter';
 
 @UseFilters(AllRpcExceptionsFilter)
-@UseGuards(AuthGuard)
+// @UseGuards(AuthGuard) // ToDo: ??????? how to guard only rpc methods?
 @RpcService()
 export class MemoryController {
   constructor(private memoryService: MemoryService) {}
 
+  @UseGuards(AuthGuard) // ToDo: ??????? how to guard only rpc methods?
   @ZodToOpenRPC({ params: PaginationSchema })
   public getMemories(
     @Body() params: PaginationDto,
@@ -21,11 +22,31 @@ export class MemoryController {
     return this.memoryService.getMemories(user.id, params.limit, params.offset);
   }
 
+  // ToDo: add admin auth guard
+  @ZodToOpenRPC({
+    params: z.object({
+      userId: z.number(),
+      limit: z.number(),
+      offset: z.number(),
+    }),
+  })
+  public getMemoriesByUser(
+    @Body() params: { userId: number; limit: number; offset: number },
+  ) {
+    return this.memoryService.getMemories(
+      params.userId,
+      params.limit,
+      params.offset,
+    );
+  }
+
+  @UseGuards(AuthGuard) // ToDo: ??????? how to guard only rpc methods?
   @ZodToOpenRPC({ params: z.object({}) })
   public deleteAllMemories(@UserInfo() user: UserInfo) {
     return this.memoryService.deleteAllMemories(user.id);
   }
 
+  @UseGuards(AuthGuard) // ToDo: ??????? how to guard only rpc methods?
   @ZodToOpenRPC({ params: z.object({ id: z.number() }) })
   public deleteMemory(
     @Body() params: { id: number },
